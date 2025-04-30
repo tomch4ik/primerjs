be9bdc8dc8a87450d9eacd78122ace89ad7aaa51
Samof_qs56@student.itstep.org <Samof_qs56@student.itstep.org>
2025-04-30 19:27:08 +0300

    task1

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-document.writeln("Hello main")
+//task1
+let TorF = false;
+do {
+    let login = prompt("Введите ФИО:");
+    let gender = prompt("Введите пол:");
+    let age = parseInt(prompt("Введите возраст:"));
+    let email = prompt("Введите email:");
+    TorF = confirm(`ФИО: ${login}\nПол: ${gender}\nВозраст: ${age}\nEmail: ${email}\n\nВсе верно?`);
+} while (!TorF);
+alert("Спасибо за информацию!");
